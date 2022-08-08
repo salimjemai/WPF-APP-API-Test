f1361117ed28c2ace7f1a29c3485e8f7b732fcce
jemas <jemas@campsystems.com>
2022-08-08 16:05:17 -0500

    Add project files.

---
+++ API-TESTER-USING-MVVM/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using API_TESTER_USING_MVVM.ViewModels;
+using Caliburn.Micro;
+using System.Windows;
+
+namespace API_TESTER_USING_MVVM
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+    }
+}
---
+++ API-TESTER-USING-MVVM/Models/SessionModel.cs
@@ -0,0 +1,14 @@
+﻿namespace API_TESTER_USING_MVVM.Models
+{
+    public class SessionModel
+    {
+        public string SchemaAlias { get; set; }
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string CwsUrl { get; set; }
+
+
+    }
+}
